df261b90f53983cb7fbbb1078de99f5dd256868f
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-09 19:47:53 +0100

    removed unneccessary styling

--- src/components/header.js
+++ src/components/header.js
@@ -7,40 +7,31 @@ import Container from './container';
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: `#fcf2cb`,
-      height: 90
+      background: `#fcf2cb`
     }}
   >
     <Container>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 1152,
-        height: `100%`,
-        padding: `0`,
-        display: `flex`,
-        alignItems: `center`
-      }}
-    >
-      <h1 style={{
-          margin: 0,
-          marginTop: -5,
-          fontFamily: `Roboto`,
-          letterSpacing: 1,
-          fontWeight: 700
+      <div
+        style={{
+          height: 90,
+          padding: `0`,
+          display: `flex`,
+          alignItems: `center`
         }}
       >
-        <Link
-          to="/"
-          style={{
-            color: `#1e4363`,
-            textDecoration: `none`,
-          }}
+        <h1 style={{ marginTop: -5 }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+          <Link
+            to="/"
+            style={{
+              color: `#1e4363`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
     </Container>
   </div>
 )
