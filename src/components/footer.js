1551e326000fbfd6a46c61f49493e933e5af86e9
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-10 00:59:15 +0100

    added terms to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,29 +8,35 @@ const Footer = () => (
   <div className="footer-box">
   <Container>
     <Centrifier>
-      <div className="footer-content-box">
-        <div className="footer-address-box">
-          <h2>Address</h2>
-          <p>33rd, Roomrstreet, <br />Roomrtown</p>
+      <div>
+        <div className="footer-content-box">
+          <div className="footer-address-box">
+            <h2>Address</h2>
+            <p>33rd, Roomrstreet, <br />Roomrtown</p>
+          </div>
+          <div className="footer-follow-box">
+            <h2>Follow us</h2>
+            <p>Facebook</p>
+            <p>LinkedIn</p>
+            <p>Twitter</p>
+            <p className="footer-github">Github</p>
+            <p>Youtube</p>
+          </div>
+          <div className="footer-company-box">
+            <h2>Company</h2>
+            <p>About</p>
+            <p className="footer-culture">Culture</p>
+            <p>Career</p>
+            <p>Press</p>
+          </div>
         </div>
-        <div className="footer-follow-box">
-          <h2>Follow us</h2>
-          <p>Facebook</p>
-          <p>LinkedIn</p>
-          <p>Twitter</p>
-          <p className="footer-github">Github</p>
-          <p>Youtube</p>
+        <div className="footer-term-box">
+          <p>Terms</p>
+          <p>Privacy</p>
+          <p>Roomr <span>©</span> 2018</p>
         </div>
-        <div className="footer-company-box">
-          <h2>Company</h2>
-          <p>About</p>
-          <p className="footer-culture">Culture</p>
-          <p>Career</p>
-          <p>Press</p>
-        </div>
-      </div>
+      </div>  
     </Centrifier>
-    
   </Container>
   </div>
 )
