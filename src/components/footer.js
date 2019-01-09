9e5e106c5f1cb6d71560b81815f238c5479ebf95
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-09 23:49:22 +0100

    created footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Container from './container';
+import Centrifier from './centrifier';
+import './footer.css';
+
+const Footer = () => (
+  <div className="footer-box">
+  <Container>
+    <Centrifier>
+      <div>hi</div>
+    </Centrifier>
+    
+  </Container>
+  </div>
+)
+
+export default Footer;
