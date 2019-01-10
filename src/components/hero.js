5467f96eedfb5ba939123c9a928658448bbe1983
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-10 23:15:19 +0100

    added the hero text and buttons - not finished yet

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './hero.css';
 
+import Container from './container';
+
 const Hero = () => (
-  <div  className="hero"></div>
+  <div  className="hero">
+    <Container>
+      <div className="hero-header-box">
+        <div className="first-sentence">
+          <p>Spend less time</p>
+          <p>looking for empty rooms.</p>
+        </div>
+        <div className="second-sentence">
+          <p>Have more time</p>
+          <p>for actual work.</p>
+        </div>
+      </div>
+      <div className="hero-buttons">
+          <p>▹ Try demo</p>
+          <p>▹ Sign up</p>
+      </div>
+    </Container>
+  </div>
 )
 
 
