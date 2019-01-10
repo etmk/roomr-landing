b35448377022998c8ba237f89855969c2e5c2aac
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-10 01:31:43 +0100

    added Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Hero from '../components/hero';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`roomr`, `room management`, `open-source`, `free`]} />  
+    <SEO title="Home" keywords={[`roomr`, `room management`, `open-source`, `free`]} />
+    <Hero />
   </Layout>
 )
 
