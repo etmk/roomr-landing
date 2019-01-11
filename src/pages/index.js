a41236cd2910f3e4931bd0a2617e73211976aad6
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-11 17:18:59 +0100

    added featureSection to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Hero from '../components/hero';
+import FeatureSection from '../components/featureSection';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`roomr`, `room management`, `open-source`, `free`]} />
     <Hero />
+    <FeatureSection />
   </Layout>
 )
 
