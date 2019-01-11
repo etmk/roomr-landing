6e6ce56dac23061ce9c84cfa8f550b64a3fcec9a
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-11 23:31:50 +0100

    added page icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         background_color: `#1e4363`,
         theme_color: `#1e4363`,
         display: `minimal-ui`,
+        icon: `src/images/roomr_icon.png`
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
