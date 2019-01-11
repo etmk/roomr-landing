c11b20c6fe1c0fcc0fef0f6039d4e86dcbf47580
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-11 21:16:57 +0100

    added feature icons

--- src/components/featureSection.js
+++ src/components/featureSection.js
@@ -6,6 +6,12 @@ import SectionHeader from './sectionHeader';
 
 import './featureSection.css';
 import triangle from "../images/feature-triangle.svg";
+import book from "../images/book_and_unbook.svg";
+import everyDevice from "../images/device_icon.svg";
+import people from "../images/people_icon.svg";
+import calendarSync from "../images/calendarsync_icon.svg";
+import analytics from "../images/analytics_icon.svg";
+import key from "../images/key_icon.svg";
 
 const FeatureSection = () => (
   <div className="feature-section">
@@ -21,28 +27,34 @@ const FeatureSection = () => (
             <div className="feature-box__card">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Book and<br></br>unbook rooms</p>
+              <img className="feature-icon" src={book} alt="book and unbook icon"/>
             </div>
             <div className="feature-box__card">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Works on<br></br>every device</p>
+              <img className="feature-icon" src={everyDevice} alt="works on every device"/>
             </div>
             <div className="feature-box__card">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Admin and<br></br>members accounts</p>
+              <img className="feature-icon" src={people} alt="member and admin accounts"/>
             </div>
           </div>
           <div className="feature-box__row">
             <div className="feature-box__card">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Calendar<br></br>integrations</p>
+              <img className="feature-icon" src={calendarSync} alt="calendar integrations"/>
             </div>
             <div className="feature-box__card">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Usage<br></br>analytics</p>
+              <img className="feature-icon" src={analytics} alt="usage analytics"/>
             </div>
             <div className="feature-box__card">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>SAML single<br></br>sign-on</p>
+              <img className="feature-icon" src={key} alt="SAML single sign-on"/>
             </div>
           </div>
         </div>
