dcd71694615bfeddf92415e10ab72e8d950cd6c8
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-11 17:37:25 +0100

    added conditional h2 color

--- src/components/sectionHeader.js
+++ src/components/sectionHeader.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './sectionHeader.css';
 
-const SectionHeader = ({ text, lineWidth }) => (
+const SectionHeader = ({ text, lineWidth, color }) => (
   <div className="section-header">
-    <h2>{text}</h2>
+    <h2
+      style={{ "color": (color === 'blue') ? "#1e4363" : "#fcf2cb"}}
+    >
+      {text}
+    </h2>
   </div>
 )
 
