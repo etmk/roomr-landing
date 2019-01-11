84abdff89fa9ec464cbdd41aad9d63eab96d2961
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-11 22:53:30 +0100

    features now render well on all devices

--- src/components/featureSection.js
+++ src/components/featureSection.js
@@ -24,34 +24,33 @@ const FeatureSection = () => (
       <Centrifier>
       <div className="feature-box">
           <div className="feature-box__row">
-            <div className="feature-box__card">
+            <div className="feature-box__card card--book">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Book and<br></br>unbook rooms</p>
               <img className="schedule-icon" src={book} alt="book and unbook icon"/>
             </div>
-            <div className="feature-box__card">
+            <div className="feature-box__card card--device">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Works on<br></br>every device</p>
               <img className="device-icon" src={everyDevice} alt="works on every device"/>
             </div>
-            <div className="feature-box__card">
+            <div className="feature-box__card card--people">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Admin and<br></br>member accounts</p>
               <img className="people-icon" src={people} alt="member and admin accounts"/>
             </div>
-          </div>
-          <div className="feature-box__row">
-            <div className="feature-box__card">
+          
+            <div className="feature-box__card card--sync">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Calendar<br></br>integrations</p>
               <img className="schedule-icon" src={calendarSync} alt="calendar integrations"/>
             </div>
-            <div className="feature-box__card">
+            <div className="feature-box__card card--analytics">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>Usage<br></br>analytics</p>
               <img className="analytics-icon" src={analytics} alt="usage analytics"/>
             </div>
-            <div className="feature-box__card">
+            <div className="feature-box__card card--key">
               <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
               <p>SAML single<br></br>sign-on</p>
               <img className="key-icon" src={key} alt="SAML single sign-on"/>
