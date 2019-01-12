a2a0e77e718c129dff19922162543282d6873a6d
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-12 19:34:32 +0100

    added sourcecode p to section

--- src/components/sourcecodeSection.js
+++ src/components/sourcecodeSection.js
@@ -5,10 +5,20 @@ import Container from './container';
 import Centrifier from './centrifier';
 
 import './sourcecodeSection.css';
+import heartIcon from '../images/heart_icon.svg';
 
 const SourcecodeSection = () => (
   <div className="sourcecode-section">
     <SectionHeader text="Sourcecode" smallLine="220px" bigLine="280px"/>
+    <Container>
+      <Centrifier>
+        <div className="sourcecode-text-box">
+          <p>We</p>
+          <img src={heartIcon} />
+          <p>Open Source</p>
+        </div>
+      </Centrifier>
+    </Container>
   </div>
 )
 
