f5856940eebbc3c5eb307fc077bbe7b14c9c637f
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-12 20:18:04 +0100

    add p tag to contact section

--- src/components/contactSection.js
+++ src/components/contactSection.js
@@ -9,6 +9,13 @@ import './contactSection.css';
 const ContactSection = () => (
   <div className="contact-section">
     <SectionHeader isBlue text="Contact" smallLine="150px" bigLine="240px"/>
+    <Container>
+      <Centrifier>
+        <div className="contact-text-box">
+          <p>Want to know more about the enterprise plan or have any other question? Do not hesitate to drop us a line through the following channels!</p>
+        </div>
+      </Centrifier>
+    </Container>
   </div>
 )
 
