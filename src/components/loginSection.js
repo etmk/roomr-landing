4409b98a69dc4c0273535d4ddfab21dcb809fd2a
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-12 18:05:42 +0100

    created more loginSection content

--- src/components/loginSection.js
+++ src/components/loginSection.js
@@ -3,10 +3,24 @@ import React from 'react';
 import './loginSection.css';
 
 import SectionHeader from './sectionHeader';
+import Container from './container';
+import Centrifier from './centrifier';
 
 const LoginSection = () => (
   <div className="login-section">
     <SectionHeader isBlue text="Sign up/Login" smallLine="240px" bigLine="320px" />
+    <Container>
+      <Centrifier>
+        <div className="login-box"></div>
+      </Centrifier>
+    </Container>
+    <Container>
+      <Centrifier>
+        <div className="info-text-box">
+          <p>Choose a plan once you are signed up.</p>
+        </div>
+      </Centrifier>
+    </Container>
   </div>
 )
 
