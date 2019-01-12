870489e54fdbc4fea409ed33cc75e0f4fd5774d2
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-12 20:24:49 +0100

    added fbicon

--- src/components/contactSection.js
+++ src/components/contactSection.js
@@ -6,6 +6,8 @@ import Centrifier from './centrifier';
 
 import './contactSection.css';
 
+import fbIcon from '../images/fb_icon.svg';
+
 const ContactSection = () => (
   <div className="contact-section">
     <SectionHeader isBlue text="Contact" smallLine="150px" bigLine="240px"/>
@@ -13,6 +15,14 @@ const ContactSection = () => (
       <Centrifier>
         <div className="contact-text-box">
           <p>Want to know more about the enterprise plan or have any other question? Do not hesitate to drop us a line through the following channels!</p>
+        </div>
+      </Centrifier>
+      <Centrifier>
+        <div className="contact-social-box">
+          <img src={fbIcon} />
+          <img src={fbIcon} />
+          <img src={fbIcon} />
+          <img src={fbIcon} />
         </div>
       </Centrifier>
     </Container>
