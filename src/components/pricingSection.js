7f4b80b74358e1e662f07e4e918dd6f0f3aa71b9
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-13 20:04:43 +0100

    added rest of pricing content

--- src/components/pricingSection.js
+++ src/components/pricingSection.js
@@ -41,10 +41,25 @@ const PricingSection = () => (
           <div className="plan-box">
             <p className="plan-name" >Pro</p>
             <p className="plan-price" >4$/room</p>
+            <p className="plan-per-month">per month</p>
+            <p>minimum 1 room</p>
+            <p className="plan-includes">Includes:</p>
+            <p className="plan-feature">all of basic</p>
+            <p className="plan-feature">calendar integrations</p>
+            <p className="plan-feature">usage analytics</p>
           </div>
           <div className="plan-box">
             <p className="plan-name">Enterprise</p>
             <p className="plan-price enterprise-plan" >Contact us</p>
+            <p className="plan-per-month">per month</p>
+            <p>minimum 1 room</p>
+            <p className="plan-includes">Includes:</p>
+            <p className="plan-feature">all of basic and pro</p>
+            <p className="plan-feature">slack plugin</p>
+            <p className="plan-feature">custom booking rules</p>
+            <p className="plan-feature">SAML single sign-on</p>
+            <p className="plan-feature">dedicated support team</p>
+            <p className="plan-feature">custom branding</p>
           </div>
         </div>
       </Centrifier>
