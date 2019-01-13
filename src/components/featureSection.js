f9ef099e088d4e09b91304aadf420d3feca7b060
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-13 16:10:37 +0100

    featureSection  mobile icons and text are now bold

--- src/components/featureSection.js
+++ src/components/featureSection.js
@@ -7,12 +7,18 @@ import SectionHeader from './sectionHeader';
 
 import './featureSection.css';
 import triangle from "../images/feature-triangle.svg";
-import book from "../images/book_and_unbook.svg";
-import everyDevice from "../images/device_icon.svg";
-import people from "../images/people_icon.svg";
-import calendarSync from "../images/calendarsync_icon.svg";
-import analytics from "../images/analytics_icon.svg";
-import key from "../images/key_icon.svg";
+import bookIcon from "../images/book_and_unbook.svg";
+import bookIconBold from "../images/book_and_unbook_bold.svg";
+import deviceIcon from "../images/device_icon.svg";
+import deviceIconBold from '../images/device_icon_bold.svg';
+import peopleIcon from "../images/people_icon.svg";
+import peopleIconBold from "../images/people_icon_bold.svg";
+import calendarSyncIcon from "../images/calendarsync_icon.svg";
+import calendarSyncIconBold from '../images/calendarsync_icon_bold.svg';
+import analyticsIcon from "../images/analytics_icon.svg";
+import analyticsIconBold from '../images/analytics_icon_bold.svg';
+import keyIcon from "../images/key_icon.svg";
+import keyIconBold from '../images/key_icon_bold.svg';
 
 const FeatureSection = () => (
   <div className="feature-section">
@@ -25,62 +31,73 @@ const FeatureSection = () => (
               <MediaQuery minWidth={601}>
                 <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
                 <p>Book and<br></br>unbook rooms</p>
+                <img className="schedule-icon" src={bookIcon} alt="book and unbook icon"/>
               </MediaQuery>
               <MediaQuery maxWidth={600}>
                 <p>Book and unbook rooms</p>
+                <img className="schedule-icon" src={bookIconBold} alt="book and unbook icon"/>
               </MediaQuery>
-              <img className="schedule-icon" src={book} alt="book and unbook icon"/>
+              
             </div>
             <div className="feature-box__card card--device">
               <MediaQuery minWidth={601}>
                 <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
                 <p>Works on<br></br>every device</p>
+                <img className="device-icon" src={deviceIcon} alt="works on every device"/>
               </MediaQuery>
               <MediaQuery maxWidth={600}>
                 <p>Works on every device</p>
+                <img className="device-icon" src={deviceIconBold} alt="works on every device"/>
               </MediaQuery>
-              <img className="device-icon" src={everyDevice} alt="works on every device"/>
             </div>
             <div className="feature-box__card card--people">
               <MediaQuery minWidth={601}>
                 <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
                 <p>Admin and<br></br>member accounts</p>
+                <img className="people-icon" src={peopleIcon} alt="member and admin accounts"/>
               </MediaQuery>
               <MediaQuery maxWidth={600}>
                 <p>Admin and member accounts</p>
+                <img className="people-icon" src={peopleIconBold} alt="member and admin accounts"/>
               </MediaQuery>
-              <img className="people-icon" src={people} alt="member and admin accounts"/>
+              
             </div>
           
             <div className="feature-box__card card--sync">
               <MediaQuery minWidth={601}>
                 <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
                 <p>Calendar<br></br>integrations</p>
+                <img className="schedule-icon" src={calendarSyncIcon} alt="calendar integrations"/>
               </MediaQuery>
               <MediaQuery maxWidth={600}>
                 <p>Calendar integrations</p>
+                <img className="schedule-icon" src={calendarSyncIconBold} alt="calendar integrations"/>
               </MediaQuery>
-              <img className="schedule-icon" src={calendarSync} alt="calendar integrations"/>
+              
             </div>
             <div className="feature-box__card card--analytics">
               <MediaQuery minWidth={601}>
                 <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
                 <p>Usage<br></br>analytics</p>
+                <img className="analytics-icon" src={analyticsIcon} alt="usage analytics"/>
               </MediaQuery>
               <MediaQuery maxWidth={600}>
                 <p>Usage analytics</p>
+                <img className="analytics-icon" src={analyticsIconBold} alt="usage analytics"/>
               </MediaQuery>
-              <img className="analytics-icon" src={analytics} alt="usage analytics"/>
+              
             </div>
             <div className="feature-box__card card--key">
               <MediaQuery minWidth={601}>
                 <img className="feature-triangle" src={triangle} alt="feature-triangle"/>
                 <p>SAML single<br></br>sign-on</p>
+                <img className="key-icon" src={keyIcon} alt="SAML single sign-on"/>
               </MediaQuery>
               <MediaQuery maxWidth={600}>
                 <p>SAML single sign-on</p>
+                <img className="key-icon" src={keyIconBold} alt="SAML single sign-on"/>
               </MediaQuery>
-              <img className="key-icon" src={key} alt="SAML single sign-on"/>
+              
             </div>
           </div>
         </div>
