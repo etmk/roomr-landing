72499eb9d571e6831fb6d878b2623b9fed2e82cf
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-13 21:39:56 +0100

    added onIndicatorClick function, pages displaylogic now handled in react instead of with mediaquery and display:none

--- src/components/pricingSection.js
+++ src/components/pricingSection.js
@@ -10,11 +10,31 @@ import './pricingSection.css';
 
 class PricingSection extends Component {
   state = {
-    monthly: true
+    monthly: true,
+    plan: 1,
+    width:''
   }
 
-  onToggle = () => {
+  componentDidMount = async () => {
+    await this.updateWidth();
+    window.addEventListener("resize", this.updateWidth);
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", this.updateWidth);
+  }
+
+
+  updateWidth = () => {
+    this.setState({ width: window.innerWidth });
+  }
+  onPriceToggle = () => {
     this.setState(prevState => ({ monthly: !prevState.monthly}));
+  }
+
+  onIndicatorClick = event => {
+    const number = parseInt(event.target.name);
+    this.setState({ plan: number});
   }
 
   render () {
@@ -30,7 +50,7 @@ class PricingSection extends Component {
           <Centrifier>
             <div className="pricing__toggle-box">
               <p>Monthly</p>
-              <div className="toggle-background" onClick={this.onToggle}>
+              <div className="toggle-background" onClick={this.onPriceToggle}>
                 <div
                   className={`toggle-foreground ${(!this.state.monthly) ? "right" : null}`}
                 >
@@ -41,51 +61,57 @@ class PricingSection extends Component {
           </Centrifier>
           <Centrifier>
             <div className="pricing__plans-box">
-              <div className="plan-box">
-                <p className="plan-name" >Basic</p>
-                <p className="plan-price" >
-                  {(this.state.monthly) ? "3": "2"}$/room
-                </p>
-                <p className="plan-per-month">per month</p>
-                <p>minimum 1 room</p>
-                <p className="plan-includes">Includes:</p>
-                <p className="plan-feature">block and unblock rooms</p>
-                <p className="plan-feature">access on any device</p>
-                <p className="plan-feature">unlimited members & admins</p>
-              </div>
-              <div className="plan-box">
-                <p className="plan-name" >Pro</p>
-                <p className="plan-price" >
-                {(this.state.monthly) ? "5": "4"}$/room
-                </p>
-                <p className="plan-per-month">per month</p>
-                <p>minimum 1 room</p>
-                <p className="plan-includes">Includes:</p>
-                <p className="plan-feature">all of basic</p>
-                <p className="plan-feature">calendar integrations</p>
-                <p className="plan-feature">usage analytics</p>
-              </div>
-              <div className="plan-box">
-                <p className="plan-name">Enterprise</p>
-                <p className="plan-price enterprise-plan" >Contact us</p>
-                <p className="plan-per-month">per month</p>
-                <p>minimum 1 room</p>
-                <p className="plan-includes">Includes:</p>
-                <p className="plan-feature">all of basic and pro</p>
-                <p className="plan-feature">slack plugin</p>
-                <p className="plan-feature">custom booking rules</p>
-                <p className="plan-feature">SAML single sign-on</p>
-                <p className="plan-feature">dedicated support team</p>
-                <p className="plan-feature">custom branding</p>
-              </div>
+              {(this.state.plan === 0  || this.state.width > 921) && 
+                <div className="plan-box">
+                  <p className="plan-name" >Basic</p>
+                  <p className="plan-price" >
+                    {(this.state.monthly) ? "3": "2"}$/room
+                  </p>
+                  <p className="plan-per-month">per month</p>
+                  <p>minimum 1 room</p>
+                  <p className="plan-includes">Includes:</p>
+                  <p className="plan-feature">block and unblock rooms</p>
+                  <p className="plan-feature">access on any device</p>
+                  <p className="plan-feature">unlimited members & admins</p>
+                </div>
+              }
+              {(this.state.plan === 1  || this.state.width > 921) && 
+                <div className="plan-box">
+                  <p className="plan-name" >Pro</p>
+                  <p className="plan-price" >
+                  {(this.state.monthly) ? "5": "4"}$/room
+                  </p>
+                  <p className="plan-per-month">per month</p>
+                  <p>minimum 1 room</p>
+                  <p className="plan-includes">Includes:</p>
+                  <p className="plan-feature">all of basic</p>
+                  <p className="plan-feature">calendar integrations</p>
+                  <p className="plan-feature">usage analytics</p>
+                </div>
+              }
+              {(this.state.plan === 2  || this.state.width > 921) && 
+                <div className="plan-box">
+                  <p className="plan-name">Enterprise</p>
+                  <p className="plan-price enterprise-plan" >Contact us</p>
+                  <p className="plan-per-month">per month</p>
+                  <p>minimum 1 room</p>
+                  <p className="plan-includes">Includes:</p>
+                  <p className="plan-feature">all of basic and pro</p>
+                  <p className="plan-feature">slack plugin</p>
+                  <p className="plan-feature">custom booking rules</p>
+                  <p className="plan-feature">SAML single sign-on</p>
+                  <p className="plan-feature">dedicated support team</p>
+                  <p className="plan-feature">custom branding</p>
+                </div>
+              }
             </div>
           </Centrifier>
           <MediaQuery maxWidth={920}>
             <Centrifier>
               <div className="pricing__swipe-indicator">
-                <div className="indicator-box"></div>
-                <div className="indicator-box"></div>
-                <div className="indicator-box"></div>
+                <div onClick={this.onIndicatorClick} name="0" className="indicator-box"></div>
+                <div onClick={this.onIndicatorClick} name="1" className="indicator-box"></div>
+                <div onClick={this.onIndicatorClick} name="2" className="indicator-box"></div>
               </div>
             </Centrifier>
           </MediaQuery>
