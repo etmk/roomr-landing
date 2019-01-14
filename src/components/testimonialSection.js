c9808cc0e3003ee9c20a6badcf52e41a71e9e563
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-14 10:59:27 +0100

    added swiping logic

--- src/components/testimonialSection.js
+++ src/components/testimonialSection.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MediaQuery from 'react-responsive';
+import Swipeable from 'react-swipeable';
 
 import SectionHeader from './sectionHeader';
 
@@ -20,7 +21,7 @@ class TestimonialSection extends Component {
     this.setState({ testimonial: number });
   }
 
-  onTriangleRightClick = event => {
+  moveRight = event => {
     if (this.state.testimonial === 2) {
       this.setState({ testimonial: 0 });
     } else {
@@ -28,7 +29,7 @@ class TestimonialSection extends Component {
     }
   }
 
-  onTriangleLeftClick = event => {
+  moveLeft = event => {
     if (this.state.testimonial === 0) {
       this.setState({ testimonial: 2 });
     } else {
@@ -45,23 +46,28 @@ class TestimonialSection extends Component {
             <div className="testimonial-box">
               <MediaQuery minWidth={700}>
                 <img
-                  onClick={this.onTriangleLeftClick}
+                  onClick={this.moveLeft}
                   alt="testimonial triangle left"
                   src={triangleLeft}
                 />
               </MediaQuery>
-              {this.state.testimonial === 0 &&
-                <p className="testimonial">We used to spend loads of time looking for rooms. Additionally, workers looking for rooms would interrupt meetings because they were not sure whether a room was vacant or not. Roomr helped us find rooms more quickly and made sure everyone was informed at all times.</p>
-              }
-              {this.state.testimonial === 1 &&
-                <p className="testimonial">Thanks to the SAML single sign-on feature we could easily sign up all our 300 employees using the login data we already had. We had some problems while setting up but the support team helped us quickly and we got it done in under half an hour. Thank guys!</p>
-              }
-              {this.state.testimonial === 2 &&
-                <p className="testimonial">We chose roomr because the usage analytics are just so helpful. We can quickly observe which rooms are used most and at what times. On top of that, the calendar integration automatically reminds everyone so forgotten meetings are now a thing of the past for us! </p>
-              }
+              <Swipeable
+                onSwipedLeft={this.moveRight}
+                onSwipedRight={this.moveLeft}
+              >
+                {this.state.testimonial === 0 &&
+                  <p className="testimonial">We used to spend loads of time looking for rooms. Additionally, workers looking for rooms would interrupt meetings because they were not sure whether a room was vacant or not. Roomr helped us find rooms more quickly and made sure everyone was informed at all times.</p>
+                }
+                {this.state.testimonial === 1 &&
+                  <p className="testimonial">Thanks to the SAML single sign-on feature we could easily sign up all our 300 employees using the login data we already had. We had some problems while setting up but the support team helped us quickly and we got it done in under half an hour. Thank guys!</p>
+                }
+                {this.state.testimonial === 2 &&
+                  <p className="testimonial">We chose roomr because the usage analytics are just so helpful. We can quickly observe which rooms are used most and at what times. On top of that, the calendar integration automatically reminds everyone so forgotten meetings are now a thing of the past for us! </p>
+                }
+              </Swipeable>
               <MediaQuery minWidth={700}>
                 <img
-                  onClick={this.onTriangleRightClick}
+                  onClick={this.moveRight}
                   alt="testimonial triangle right"
                   src={triangleRight}
                 />
