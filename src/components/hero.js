6c9978fe19cd38e3e97892d5b0283f96c4532161
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-16 15:36:59 +0100

    putting mediaQuery into extra div to see if that fixes the issue

--- src/components/hero.js
+++ src/components/hero.js
@@ -9,11 +9,13 @@ import './hero.css';
 const Hero = ({ heroImage }) => (
   <div className="hero">
     <Container>
+    <div>
     <MediaQuery minWidth={501}>
       <div className="hero__image-box">
           <Img className="hero__image" fluid={heroImage} />
       </div>
     </MediaQuery>
+    </div>
       <div className="hero-header-box">
         <div className="first-sentence">
           <p>Spend less time</p>
